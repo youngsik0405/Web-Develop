cc55313142738485e433ecb5d36ed679fb54c8dd
Youngsik Yoon <dudtlr6512@naver.com>
2025-05-11 15:04:08 +0900

    0511 자바스크립트

---
+++ Web-Develop6/app.js
@@ -0,0 +1,10 @@
+let age = 33;
+let userName = 'Youngsik';
+let hobbies = ['Sports', 'Cooking', 'Reading'];
+let job = { 
+    title: 'Developer', 
+    place: 'New York', 
+    salary: 50000 
+};
+
+alert(hobbies[0]);
