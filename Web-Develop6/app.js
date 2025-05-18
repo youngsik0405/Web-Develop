a5662b6e40e969f4a92d3bab87746d3c89fd71fc
Youngsik Yoon <dudtlr6512@naver.com>
2025-05-18 16:07:14 +0900

    DOM 시작

--- Web-Develop6/app.js
+++ Web-Develop6/app.js
@@ -17,9 +17,18 @@ function calculateAdultYears(userAge) {
 
  
 totalAdultYears = calculateAdultYears(age);
-alert(totalAdultYears);
+console.log(totalAdultYears);
 
 age = 45;
 totalAdultYears = calculateAdultYears(age);
 
-alert(totalAdultYears);
+console.log(totalAdultYears);
+
+let person = {
+    name: 'Youngsik', // Property
+    greet() { // Method
+        console.log('Hello!');
+    }
+};
+
+person.greet();
