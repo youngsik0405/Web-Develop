5974521e52c7cf6303eec06733e4e7c828a0275c
Youngsik Yoon <dudtlr6512@naver.com>
2025-05-22 21:24:51 +0900

    0522 DOM

--- Web-Develop7/app.js
+++ Web-Develop7/app.js
@@ -1,6 +1,11 @@
-document.body.children[1].children[0].href = 'https://google.com';
+// document.body.children[1].children[0].href = 'https://google.com';
 
 // console.dir(document);
 // alert();
 // window.alert();
 
+let anchorElement = document.getElementById('external-link');
+anchorElement.href = 'https://google.com';
+
+anchorElement = document.querySelector('#external-id');
+anchorElement.href = 'https://academind.com';
